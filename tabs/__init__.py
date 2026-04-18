29b7909781deeaf69b804b3eb498a614b28a7fbe
Apple831 <beautifuappleapple@gmail.com>
2026-04-18 14:51:32 +0800

    Update __init__.py

--- tabs/__init__.py
+++ tabs/__init__.py
@@ -1,1 +1,11 @@
 # tabs package
+from . import (
+    tab_index,
+    tab_beat,
+    tab_buy_scan,
+    tab_sell_scan,
+    tab_analysis,
+    tab_backtest,
+    tab_walkforward,
+    tab_regime_matrix,
+)
